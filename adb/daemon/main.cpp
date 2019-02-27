omnirom/android_system_core_new

Make adb use a custom prop for adb root
Change-Id: Ie2b8c5e9d75371da43305f003607869370bbd1a4

adb/daemon/main.cpp
@@ -86,7 +86,7 @@ static bool should_drop_privileges() {
     // Drop privileges if ro.secure is set...
     bool drop = ro_secure;
 
-    property_get("service.adb.root", value, "");
+    property_get("lineage.service.adb.root", value, "");
     bool adb_root = (strcmp(value, "1") == 0);
     bool adb_unroot = (strcmp(value, "0") == 0);
 

adb/services.cpp
@@ -92,7 +92,7 @@ void restart_root_service(int fd, void *cookie) {
             return;
         }
 
-        property_set("service.adb.root", "1");
+        property_set("lineage.service.adb.root", "1");
         WriteFdExactly(fd, "restarting adbd as root\n");
         adb_close(fd);
     }
@@ -103,7 +103,7 @@ void restart_unroot_service(int fd, void *cookie) {
         WriteFdExactly(fd, "adbd not running as root\n");
         adb_close(fd);
     } else {
-        property_set("service.adb.root", "0");
+        property_set("lineage.service.adb.root", "0");
         WriteFdExactly(fd, "restarting adbd as non root\n");
         adb_close(fd);
     }

shinobizero/platform_system_core

adb/daemon/main.cpp
@@ -86,7 +86,7 @@ static bool should_drop_privileges() {
     // Drop privileges if ro.secure is set...
     bool drop = ro_secure;
 
-    property_get("service.adb.root", value, "");
+    property_get("cm.service.adb.root", value, "");
     bool adb_root = (strcmp(value, "1") == 0);
     bool adb_unroot = (strcmp(value, "0") == 0);
 

adb/services.cpp
@@ -92,7 +92,7 @@ void restart_root_service(int fd, void *cookie) {
             return;
         }
 
-        property_set("service.adb.root", "1");
+        property_set("cm.service.adb.root", "1");
         WriteFdExactly(fd, "restarting adbd as root\n");
         adb_close(fd);
     }
@@ -103,7 +103,7 @@ void restart_unroot_service(int fd, void *cookie) {
         WriteFdExactly(fd, "adbd not running as root\n");
         adb_close(fd);
     } else {
-        property_set("service.adb.root", "0");
+        property_set("cm.service.adb.root", "0");
         WriteFdExactly(fd, "restarting adbd as non root\n");
         adb_close(fd);
     }